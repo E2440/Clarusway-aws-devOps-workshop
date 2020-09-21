ef085b3de19a493aa81ed9f86a7490e4dde80c7a
Mehmet <hohmanfred2@gmail.com>
2020-09-21 18:55:37 +0200

    proje-3

--- aws/projects/001-roman-numerals-converter/app.py
+++ aws/projects/001-roman-numerals-converter/app.py
@@ -7,11 +7,11 @@ def index():
     if request.method == 'POST':    
         user_input = request.form["number"]
         if user_input.isalpha() or user_input == "0":
-            return render_template("index.html", developer_name = "E2227", not_valid= True)
+            return render_template("index.html", developer_name = "E2440", not_valid= True)
         else:
             user_input = int(user_input)
         if user_input >= 4000 or user_input < 1 :
-            return render_template("index.html", developer_name = "E2227", not_valid= True)
+            return render_template("index.html", developer_name = "E2440", not_valid= True)
         else:
             val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
             syb = ["M", "CM", "D", "CD","C", "XC", "L", "XL","X", "IX", "V", "IV", "I"]
